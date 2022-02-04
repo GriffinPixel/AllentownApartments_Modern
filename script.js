6e85434bc888d87e6c274a243f5b2e2ecd6f1492
GriffinPixel <griffinpixelbot@gmail.com>
2022-02-04 18:29:53 +0700

    initial commit

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ $(window).scroll(function () {
     if ($(this).scrollTop() > 106) {
         $(".index-title-wrapper").show(80);
         $(".griffinpixel-wrapper").show(80);
+        $(".index-content").addClass("index-content-long");
     }
     else {
         $(".index-title-wrapper").hide(80);
         $(".griffinpixel-wrapper").hide(80);
+        $(".index-content").removeClass("index-content-long");
     }
 });
+
